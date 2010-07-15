b5c5afabd536e314a89edcb8de8a2060644d3506
JMehring <JMehring@localhost>
2010-07-15 03:25:44 +0000

    Initial changes for drafts support
    svn path=/plone.app.drafts/branches/jmehring-drafts/; revision=37799

--- plone/app/drafts/dexterity.py
+++ plone/app/drafts/dexterity.py
@@ -22,6 +22,122 @@ from plone.app.drafts.interfaces import IDexterityDraftContainer
 from plone.app.drafts.interfaces import IDexterityDraftItem
 #from experimental.dexterityz3cformdrafts.drafts import setupDraft
 
+################################################################################
+
+from zope.component import queryUtility
+
+from plone.app.drafts.interfaces import IDraftStorage
+from plone.app.drafts.interfaces import ICurrentDraftManagement
+
+from plone.app.drafts.utils import syncDraft
+from plone.app.drafts.utils import getCurrentDraft
+from plone.app.drafts.utils import getDefaultKey
+
+from Acquisition import aq_parent
+from Products.CMFCore.utils import getToolByName
+
+# Helper methods
+
+# TODO:  Get rid of archtype specific stuff here
+def getDexterityObjectKey(context):
+    """Get a key for an Dexterity object. This will be a string
+    representation of its intid, unless it is in the portal_factory, in
+    which case it'll be the a string like
+    "${parent_intid}:portal_factory/${portal_type}"
+    """
+    
+    portal_factory = getToolByName(context, 'portal_factory', None)
+    if portal_factory is None or not portal_factory.isTemporary(context):
+        return getDefaultKey(context)
+    
+    tempFolder = aq_parent(context)
+    folder = aq_parent(aq_parent(tempFolder))
+    
+    defaultKey = getDefaultKey(folder)
+    if defaultKey is None:
+        # probably the portal root
+        defaultKey = '0'
+    
+    return "%s:%s" % (defaultKey, tempFolder.getId(),)
+
+# Main event handlers
+
+##def beginDrafting(context, event):
+##    """When we enter the edit screen, set up the target key and draft cookie
+##    path. If there is exactly one draft for the given user id and target key,
+##    consider that to be the current draft. Also mark the request with
+##    IDrafting if applicable.
+##    """
+##    
+##    storage = queryUtility(IDraftStorage)
+##    if storage is None or not storage.enabled:
+##        return
+##    
+##    request = getattr(context, 'REQUEST', None)
+##    if request is None:
+##        return
+##    
+##    current = ICurrentDraftManagement(request)
+##    
+##    # Update target key regardless - we could have a stale cookie
+##    current.targetKey = getDexterityObjectKey(context)
+##    
+##    if current.draftName is None:
+##        drafts = storage.getDrafts(current.userId, current.targetKey)
+##        if len(drafts) == 1:
+##            current.draftName = tuple(drafts.keys())[0]
+##    
+##    # Save the path now so that we can use it again later, even on URLs 
+##    # (e.g. in AJAX dialogues) that are below this path.
+##    current.path = current.defaultPath
+##    
+##    current.mark()
+##    current.save()
+    
+def syncDraftOnSave(context, event):
+    """When the edit form is saved, sync the draft (if set) and discard it.
+    Also discard the drafting cookies.
+    """
+    
+    storage = queryUtility(IDraftStorage)
+    if storage is None or not storage.enabled:
+        return
+    
+    request = getattr(context, 'REQUEST', None)
+    if request is None:
+        return
+    
+    draft = getCurrentDraft(request)
+    if draft is not None:
+        syncDraft(draft, context)
+    
+    current = ICurrentDraftManagement(request)    
+    if current.userId and current.targetKey:
+        storage.discardDrafts(current.userId, current.targetKey)
+    
+    current.discard()
+    
+def discardDraftsOnCancel(context, event):
+    """When the edit form is cancelled, discard any unused drafts and
+    remove the drafting cookies.
+    """
+    
+    storage = queryUtility(IDraftStorage)
+    if storage is None or not storage.enabled:
+        return
+    
+    request = getattr(context, 'REQUEST', None)
+    if request is None:
+        return
+    
+    current = ICurrentDraftManagement(request)
+    
+    if current.userId and current.targetKey:
+        storage.discardDrafts(current.userId, current.targetKey)
+    
+    current.discard()
+    
+################################################################################
 
 # NOT YET USED
 class DexterityDraftError( Exception ):
@@ -39,8 +155,7 @@ def addBegun(form, event):
 ##    if IDexterityDraft.providedBy(form.context):
 ##        form.ignoreContext = False
         
-    #draftRequestForm(form, event)
-    return
+    draftRequestForm(form, event)
 
 def editBegun(form, event):
     fti = zope.component.queryUtility( IDexterityFTI, name=form.portal_type )
@@ -134,74 +249,140 @@ def draftRequestForm(form, event):
     if not 'plone.app.drafts.interfaces.IDexterityDraftable' in fti.behaviors:
         return context
         
-    request = getattr(form.context, 'REQUEST', None)
-    if request is None:
-        return
+    #request = getattr(form.context, 'REQUEST', None)
+    #if request is None:
+    #    return
+    request = form.request
+    alsoProvides( request, IDexterityDraft )
     
-    setupDraft( form.context, request, event )
+    content = form.getContent()
+    
+    #
+    # TODO:  Consider a better way of adding behaviors to form.  Problem is
+    #        we can't get it from ++add++ form since object has not yet been
+    #        created. 
+    #        Maybe create a utility for dexterity that could automatically
+    #        mark a form; or request -- makes things a wee messy :(
+    #
+    from z3c.form.interfaces import IAddForm
+    if IAddForm.providedBy( form ):
+        alsoProvides( form, IDexterityDraftSubmitBehavior )
+        alsoProvides( form, IDexterityDraftCancelBehavior )
+        
+        from plone.app.dexterity.behaviors.drafts import IDexterityDraftSaveBehavior
+        if 'plone.app.dexterity.behaviors.drafts.IDexterityDraftSaveBehavior' in fti.behaviors:
+            alsoProvides( form, IDexterityDraftSaveBehavior )
+ 
+    #setupDraft( form.context, request, event )
+    setupDraft( content, request, event )
     
     draft = getCurrentDraft(request, create=True)
     if draft is None:
         return
     
-    draft_request_form = getattr( draft, '_form', {} ).copy()
-    
-    # TEMP (REMOVE ANY button actions from draft
-    for key, value in draft_request_form.items():
-        if key.startswith( 'form.buttons' ):
-            draft_request_form.pop( key )
-            
-        
     from zope.schema import getFields
     from ZPublisher.HTTPRequest import FileUpload
     from z3c.form import interfaces
     
-    fields = getFields( fti.lookupSchema() )
-    for key, value in request.form.items():
-        # Don't save button actions
-        if key.startswith( 'form.buttons' ):
-            continue
-        
-##        if draft_request_form.get(key+'.prevent_override', None):
-##            # Dont save the form value (used for ajax widgets)
-##            if not request.form.has_key( value + '.override' ):
-##                continue 
-        
-        # Save the converted value in draft object since some may be 
-        # instancemethod objects (FileUpload is ) which can not be pickled
-        field = fields.get(key[key.rfind('.')+1:], None)
-        if field is not None:
-            #widget = zope.component.getMultiAdapter( (field, request), interfaces.IFieldWidget )            
-            #extracted_value = widget.extract(value)
-            #converted_value = interfaces.IDataConverter(widget).toFieldValue( value )
-            #converted_value = interfaces.IDataConverter(widget).toWidgetValue( value )
+    #draft_request_form = getattr( draft, '_form', {} ).copy()
+    #
+    # TODO:  save request as attrs not in a dict on draft for compatibility
+    #        with other processes using draft for something different?
+    #
+    draft_request_form = getattr( draft, '_form', {} )
+    new_request_form = {}
+    button_actions = {}
+    
+    #
+    # Edit form must use ignoreContext = True so values are loaded from
+    # request; or change named image to grab from request first; if exists?
+    # (will need to populate draft with object data initially)
+    if request.form.has_key('-C') and len(draft_request_form) == 0:
+        if not interfaces.IEditForm.providedBy( form ):
+            # Nothing to do 
+            return
+        # Populate request from content object (initial load)
+        if len( draft_request_form ) == 0:
+            schema = getFields( fti.lookupSchema() )
             
-            # TODO:  check to see if value is instancemethod
+            for field_name, field_schema in schema.items():
+                attr = getattr( content, field_name, NotFound )
+                if attr is not NotFound:
+                    draft_request_form[field_name] = attr 
+
+            for key, value in draft_request_form.items():
+                new_request_form[key] = value
+    # Populate request from draft if its empty
+    elif request.form.has_key('-C') and len(draft_request_form) != 0:
+        for key, value in draft_request_form.items():
+            new_request_form[key] = value
+    else:    
+        fields = getFields( fti.lookupSchema() )
+        for key, value in request.form.items():
+            # Don't save button actions (or recursion can happen)
+            if key.startswith( 'form.buttons' ):
+                button_actions[key] = value
+                continue
             
-            # Don't overwite blank FileUpload fields
-            if isinstance(value, FileUpload):
-                widget = zope.component.getMultiAdapter( (field, request), interfaces.IFieldWidget )            
-                converted_value = interfaces.IDataConverter(widget).toFieldValue( value )
+            # Don't save empty form indicator
+            if key == '-C':
+                continue
+            
+            # Save the converted value in draft object since some may be 
+            # instancemethod objects (FileUpload is ) which can not be pickled
+            field = fields.get(key[key.rfind('.')+1:], None)
+            if field is not None:
+                #widget = zope.component.getMultiAdapter( (field, request), interfaces.IFieldWidget )            
+                #extracted_value = widget.extract(value)
+                #converted_value = interfaces.IDataConverter(widget).toFieldValue( value )
+                #converted_value = interfaces.IDataConverter(widget).toWidgetValue( value )
                 
-                if converted_value is None: 
-                    if draft_request_form.has_key( key ):
-                        draft_value = draft_request_form.get(key)
-                        if draft_value is not None:
+                # Need to handle INamed fields special since we can not
+                # store the FileUpload object directly because it is
+                # an instancemethod
+                from plone.namedfile.interfaces import INamedField
+                from types import InstanceType
+                if INamedField.providedBy( field ):
+                    # More than likely ajax inline validation sent a string
+                    # containing either the filename or ''
+                    if not isinstance(value, FileUpload):
+                        value = draft_request_form.get(key, None)
+                        if value is None:
                             continue
+                    else:
+                        widget = zope.component.getMultiAdapter( (field, request), interfaces.IFieldWidget )            
+                        converted_value = interfaces.IDataConverter(widget).toFieldValue( value )
                         
-                value = converted_value #This may be enough to update request.form?
+                        # Reload image from draft if it exists
+                        if converted_value is None: 
+                            converted_value = draft_request_form.get(key, None)
+                            #if converted_value is not None:
+                            if converted_value is None:
+                                continue
+                                
+                        value = converted_value #This may be enough to update request.form?
+                        
+                # Can not save instancemethods, so skip it (dunno where this
+                # could happen other than INamed objects (handled above)
+                elif type(value) == InstanceType:
+                    continue
+                
+            new_request_form[key] = value
             
-        draft_request_form[key] = value
-        
-        #DEBUG; test only to see if this will work for preview
-        # (set attribute directly; not just on form)
-        # if this works; dont set anything not in schema
-        #setattr( draft, key[13:], value )
+            #DEBUG; test only to see if this will work for preview
+            # (set attribute directly; not just on form)
+            # if this works; dont set anything not in schema
+            #setattr( draft, key[13:], value )
         
     # Re-wrtie the form from draft incase anything was changed since last request
-    setattr( draft, '_form', draft_request_form )
-    form.context.REQUEST.form = draft_request_form.copy()
-    form.request.form = form.context.REQUEST.form #may not need this; same object?
+    setattr( draft, '_form', new_request_form.copy() )
+    #form.context.REQUEST.form = new_request_form #.copy()
+    #form.request.form = form.context.REQUEST.form #may not need this; same object?
+    request.form = new_request_form
+    
+    # Add back in any button actions that were removed to request
+    for key, value in button_actions.items():
+        request.form[key] = value
     
     return
 
@@ -211,8 +392,6 @@ from zope.component import queryUtility
 
 from plone.app.drafts.interfaces import IDraftStorage
 from plone.app.drafts.interfaces import ICurrentDraftManagement
-from plone.app.drafts.archetypes import getArchetypesObjectKey
-
 
 # Helper methods
 def setupDraft(context, request=None, event=None):
@@ -231,7 +410,7 @@ def setupDraft(context, request=None, event=None):
     current = ICurrentDraftManagement(request)
     
     # Update target key regardless - we could have a stale cookie
-    current.targetKey = getArchetypesObjectKey(context)
+    current.targetKey = getDexterityObjectKey(context)
     
     if current.userId is None:
         # More than likely user has not yet been validated, so try to figure 
@@ -306,27 +485,34 @@ class PopulateContentFromDraftSyncer(object):
     # notify of object changes
     notify = True
     
+    source = None
+    target = None
+    portal_type = None
+    
     def __init__(self, source, target):
-        self.source = source.context
+        #self.source = source.context
+        self.source = source
         self.target = target
+        self.portal_type = getattr( target, 'portal_type', None )
 
     def __call__(self):
         changes = {}
         
-        fti = queryUtility(IDexterityFTI, self.target.portal_type)
+        #fti = queryUtility(IDexterityFTI, self.target.portal_type)
+        fti = queryUtility(IDexterityFTI, self.portal_type)
         schema = getFields( fti.lookupSchema() )
         
         for field_name, field_schema in schema.items():
             source_attr = getattr( self.source, field_name, NotFound )
             if source_attr is not NotFound:
                 target_attr = getattr( self.target, field_name, NotFound )
-                if target_attr is not NotFound:
-                    if (source_attr != target_attr 
-                        or zope.schema.interfaces.IObject.providedBy(field_schema)):
-                        setattr( self.target, field_name, getattr(self.source, field_name) )
-                        
-                        # Record the change using information required later
-                        changes.setdefault(field_schema.interface, []).append(field_name)
+                #if target_attr is not NotFound:
+                if (source_attr != target_attr 
+                    or zope.schema.interfaces.IObject.providedBy(field_schema)):
+                    setattr( self.target, field_name, getattr(self.source, field_name) )
+                    
+                    # Record the change using information required later
+                    changes.setdefault(field_schema.interface, []).append(field_name)
 
         if self.notify:
             if changes:
@@ -347,7 +533,13 @@ class PopulateDraftFromContextSyncer(PopulateContentFromDraftSyncer):
     adapts(IDexterityContent, IDexterityDraft)
     
     def __init__(self, source, target):
-        self.source = source
-        self.target = target
+        super(PopulateDraftFromContextSyncer, self).__init__(source,target)
+        
+        portal_type = getattr( source, 'portal_type', None )
+        
+        # Don't overwrite default set portal type if its None
+        if portal_type is not None:
+            self.portal_type = portal_type
+            
         self.notify = False
         
