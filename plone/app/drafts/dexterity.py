9c90109aad9a81448cbef175eed0f895d631def0
JMehring <JMehring@localhost>
2010-08-05 07:02:30 +0000

    removed .pyc
    svn path=/plone.app.drafts/branches/jmehring-drafts/; revision=38530

--- plone/app/drafts/dexterity.py
+++ plone/app/drafts/dexterity.py
@@ -39,6 +39,125 @@ from Products.CMFCore.utils import getToolByName
 
 # Helper methods
 
+# TODO:  Get rid of archetype only stuff
+def getDexterityObjectKey(context):
+    """Get a key for a Dexterity object. This will be a string
+    representation of its intid, unless it is in the portal_factory, in
+    which case it'll be the a string like
+    "${parent_intid}:portal_factory/${portal_type}"
+    """
+    
+    portal_factory = getToolByName(context, 'portal_factory', None)
+    if portal_factory is None or not portal_factory.isTemporary(context):
+        return getDefaultKey(context)
+    
+    tempFolder = aq_parent(context)
+    folder = aq_parent(aq_parent(tempFolder))
+    
+    defaultKey = getDefaultKey(folder)
+    if defaultKey is None:
+        # probably the portal root
+        defaultKey = '0'
+    
+    return "%s:%s" % (defaultKey, tempFolder.getId(),)
+
+# Main event handlers
+
+def beginDrafting(context, event):
+    """When we enter the edit screen, set up the target key and draft cookie
+    path. If there is exactly one draft for the given user id and target key,
+    consider that to be the current draft. Also mark the request with
+    IDrafting if applicable.
+    """
+    
+    storage = queryUtility(IDraftStorage)
+    if storage is None or not storage.enabled:
+        return
+    
+    request = getattr(context, 'REQUEST', None)
+    if request is None:
+        return
+    
+    current = ICurrentDraftManagement(request)
+    
+    # Update target key regardless - we could have a stale cookie
+    current.targetKey = getDexterityObjectKey(context)
+    
+    if current.draftName is None:
+        drafts = storage.getDrafts(current.userId, current.targetKey)
+        if len(drafts) == 1:
+            current.draftName = tuple(drafts.keys())[0]
+    
+    # Save the path now so that we can use it again later, even on URLs 
+    # (e.g. in AJAX dialogues) that are below this path.
+    current.path = current.defaultPath
+    
+    current.mark()
+    current.save()
+    
+def syncDraftOnSave(context, event):
+    """When the edit form is saved, sync the draft (if set) and discard it.
+    Also discard the drafting cookies.
+    """
+    
+    storage = queryUtility(IDraftStorage)
+    if storage is None or not storage.enabled:
+        return
+    
+    request = getattr(context, 'REQUEST', None)
+    if request is None:
+        return
+    
+    draft = getCurrentDraft(request)
+    if draft is not None:
+        syncDraft(draft, context)
+    
+    current = ICurrentDraftManagement(request)    
+    if current.userId and current.targetKey:
+        storage.discardDrafts(current.userId, current.targetKey)
+    
+    current.discard()
+    
+def discardDraftsOnCancel(context, event):
+    """When the edit form is cancelled, discard any unused drafts and
+    remove the drafting cookies.
+    """
+    
+    storage = queryUtility(IDraftStorage)
+    if storage is None or not storage.enabled:
+        return
+    
+    request = getattr(context, 'REQUEST', None)
+    if request is None:
+        return
+    
+    current = ICurrentDraftManagement(request)
+    
+    if current.userId and current.targetKey:
+        storage.discardDrafts(current.userId, current.targetKey)
+    
+    current.discard()
+
+################################################################################
+
+# NOT YET USED
+class DexterityDraftError( Exception ):
+    """
+    """
+    pass
+
+from plone.app.drafts.interfaces import IDraftStorage
+from plone.app.drafts.interfaces import ICurrentDraftManagement
+
+from plone.app.drafts.utils import syncDraft
+from plone.app.drafts.utils import getCurrentDraft
+from plone.app.drafts.utils import getDefaultKey
+
+from Acquisition import aq_parent
+from Products.CMFCore.utils import getToolByName
+
+# Helper methods
+
 # TODO:  Get rid of archtype specific stuff here
 def getDexterityObjectKey(context):
     """Get a key for an Dexterity object. This will be a string
@@ -168,8 +287,6 @@ def setupDraft(context, request=None, portal_type=None):
     
     # Update target key regardless - we could have a stale cookie
     current.targetKey = getDexterityObjectKey(context)
-    if portal_type is not None:
-        current.targetKey += ':' + portal_type
     
     if current.userId is None:
         # More than likely user has not yet been validated, so try to figure 
