45ae2a6010dfc0a6aef6a8c3c9ffd6d7fc3089a2
Peter Mathis <peter.mathis@kombinat.at>
2021-09-22 07:42:09 +0200

    remove py2.7 information

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(
         "Framework :: Plone :: 6.0",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
